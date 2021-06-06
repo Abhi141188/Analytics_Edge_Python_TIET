4eab1bee97ba04e5db5dc2ac61b9eb807429958e
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2021-06-06 14:56:02 +0530

    Prgrm-0

---
+++ PythonBatch-Intro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 14:37:16 2021
+
+@author: pc
+"""
+
+X = [1,2,3,4,5]
+Y = [2,4,6,8,10]
+print(X)
+print(Y)
+import matplotlib.pyplot as plt
+plt.plot(X,Y)
